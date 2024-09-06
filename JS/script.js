61f591abfc4720ba89516f620a42fe8612b4016b
Roman Demianenko <Roman.Demianenko@student.souepl.cz>
2024-09-06 11:32:24 +0200

    Entering_JS_0

---
+++ JS/script.js
@@ -0,0 +1,8 @@
+//let input = document.getElementById("Input");   /* */
+
+function Add(){
+    let input = document.getElementById("Input");
+    document.getElementById("output").innerHTML = input.value;
+
+    
+}
